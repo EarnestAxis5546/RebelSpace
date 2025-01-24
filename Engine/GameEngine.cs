0e0a0ba43913cdb630ebecb6472944a680522bdd
Earni_cj <155811009+EarnestAxis5546@users.noreply.github.com>
2025-01-24 22:10:12 +0200

    Update GameEngine.cs

--- Engine/GameEngine.cs
+++ Engine/GameEngine.cs
@@ -1,35 +1,41 @@
+// File: Engine/GameEngine.cs
+using System;
 using System.Collections.Generic;
+using System.Windows.Forms; // Добавьте это пространство имен
 
-public class GameEngine
+namespace RebelSpace.Engine
 {
-    private Renderer renderer;
-    private PhysicsEngine physicsEngine;
-    private List<GameObject> gameObjects;
-
-    public GameEngine()
+    public class GameEngine
     {
-        this.renderer = new Renderer(800, 600);
-        this.physicsEngine = new PhysicsEngine();
-        this.gameObjects = new List<GameObject>();
+        private Renderer renderer;
+        private PhysicsEngine physicsEngine;
+        private List<GameObject> gameObjects;
 
-        // Load game objects, for example:
-        // Bitmap playerTexture = new Bitmap("Assets/Textures/Characters/player.png");
-        // GameObject player = new GameObject(playerTexture, new PointF(100, 100), new PointF(0, 0));
-        // gameObjects.Add(player);
-    }
-
-    public void Run()
-    {
-        Application.Run(renderer);
-    }
-
-    public void Update()
-    {
-        physicsEngine.Update(gameObjects);
-        
-        foreach (var gameObject in gameObjects)
+        public GameEngine()
         {
-            gameObject.Update();
+            this.renderer = new Renderer(800, 600);
+            this.physicsEngine = new PhysicsEngine();
+            this.gameObjects = new List<GameObject>();
+
+            // Load game objects, for example:
+            // Bitmap playerTexture = new Bitmap("Assets/Textures/Characters/player.png");
+            // GameObject player = new GameObject(playerTexture, new PointF(100, 100), new PointF(0, 0));
+            // gameObjects.Add(player);
+        }
+
+        public void Run()
+        {
+            Application.Run(renderer);
+        }
+
+        public void Update()
+        {
+            physicsEngine.Update(gameObjects);
+            
+            foreach (var gameObject in gameObjects)
+            {
+                gameObject.Update();
+            }
         }
     }
 }
